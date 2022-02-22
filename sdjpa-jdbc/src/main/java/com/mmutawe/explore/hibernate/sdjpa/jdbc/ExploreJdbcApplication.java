964a20e554d0bdd3d7c626352a9b15b9f6a42af2
mmutawe <mahmoudmutawe@gmail.com>
2022-02-22 10:24:59 -0600

    Initial commit

---
+++ sdjpa-jdbc/src/main/java/com/mmutawe/explore/hibernate/sdjpa/jdbc/ExploreJdbcApplication.java
@@ -0,0 +1,11 @@
+package com.mmutawe.explore.hibernate.sdjpa.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreJdbcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreJdbcApplication.class, args);
+    }
+}
