3a8c1abf9f35a88e02e5ee9867bedf40025a6677
mmutawe <mahmoudmutawe@gmail.com>
2022-03-01 08:43:09 -0600

    init explore hibernate dao project

---
+++ sdjpa-hibernate-dao/src/main/java/com/mmutawe/explore/hibernate/sdjpa/hibernate/dao/ExploreHibernateDaoApplication.java
@@ -0,0 +1,11 @@
+package com.mmutawe.explore.hibernate.sdjpa.hibernate.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreHibernateDaoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreHibernateDaoApplication.class, args);
+    }
+}
